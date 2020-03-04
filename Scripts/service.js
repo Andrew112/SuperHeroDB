d2bac70642de7d5d7c7a240b7662d04c59d99c48
Andrew112-Andrew Coleman <andrew.coleman330@gmail.com>
2020-03-04 08:11:33 -0600

    First Commit

---
+++ Scripts/service.js
@@ -0,0 +1,12 @@
+﻿function getAllHeroes() {
+    $.ajax({
+        url: "Service / SuperHeroService.svc / GetAllHeroes",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            heroes = result;
+            drawHeroTable(result); //"This is the first function in app.js", calls the main function method for this class.
+            //console.log(result); //this test data inside of console without the need for "html output".
+        }
+    });
+}
---
+++ Service/SuperHeroService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using System.Text;
+using System.Web.Script.Services;
+
+namespace SuperHeroDB.Service
+{
+	
+		[ServiceContract(Namespace = "")]
+		[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+		public class SuperHeroService
+		{
+			//[OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+			//public string DoWork()
+			//{
+			//    return "This is the SuperHeroDB service!";
+			//}
+
+			//Documents/VisualStudio2015/MyProjects
+
+			[OperationContract]
+			[WebGet(ResponseFormat = WebMessageFormat.Json)]
+			public List<SuperHero> GetAllHeroes()
+			{
+				return Data.SuperHeroes;
+
+			}
+
+			[OperationContract]
+			[WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetHero/{id}")]
+			public SuperHero GetHero(string id)
+			{
+				return Data.SuperHeroes.Find(sh => sh.Id == int.Parse(id));
+
+			}
+		}
+	}
+
+
